0c2a22c27ef1e76cdb324539992301ceeeed9304
wanjun.shi <wanjun.shi@transsion.com>
2024-01-05 18:18:34 +0800

    add: 普通函数和箭头函数中的this指向

--- 9、常用工具类函数封装/utils.js
+++ 9、常用工具类函数封装/utils.js
@@ -18,6 +18,26 @@ export const deepCopy = (obj) => {
     return result;
 }
 
+// 深度克隆
+export const deepClone = (obj) => {
+    let objClone = Array.isArray(obj) ? [] : {};
+    if (obj && typeof obj === 'object') {
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                // 判断ojb子元素是否为对象，如果是，递归复制
+                if (obj[key] && typeof obj[key] === 'object') {
+                    objClone[key] = deepClone(obj[key]);
+                } else {
+                    // 如果不是，简单复制
+                    objClone[key] = obj[key];
+                }
+            }
+        }
+    }
+    return objClone;
+}
+
+
 /**
  * 格式化时间戳
  * @param {*} timestamp 
